50b32aff0c2eea5a84964098815ef256feeb4915
Pradeep Vishwakarma <90743112+pradeep01234@users.noreply.github.com>
2023-12-16 02:56:09 +0000

    done

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from flask import Flask, jsonify, request
+import os
  
  
 app = Flask(__name__)
@@ -51,4 +52,4 @@ def custom_qna():
         })
    
    
-app.run(debug=True)
+app.run(debug=True,port=os.environ['PORT'])
